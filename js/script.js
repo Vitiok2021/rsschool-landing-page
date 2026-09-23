32d8584a023f9783a58d2263f28a3dccfb309d3b
vitiok2021 <viktorpts1vol@gmail.com>
2026-09-23 18:16:21 +0300

    feat: complete burger menu functionality

--- js/script.js
+++ js/script.js
@@ -19,7 +19,22 @@ const burgerBtn = document.querySelector('.icon-menu')
 const body = document.querySelector('body')
 const burgerBody = document.querySelector('.menu-header')
 
+const menuLinks = document.querySelectorAll('.menu-header__link, .actions-header__menu')
+
 burgerBtn.addEventListener('click', () => {
   body.classList.toggle('menu-open')
   body.classList.toggle('body-lock')
+})
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    body.classList.remove('menu-open')
+    body.classList.remove('body-lock')
+  })
+})
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    body.classList.remove('menu-open')
+    body.classList.remove('body-lock')
+  }
 })
